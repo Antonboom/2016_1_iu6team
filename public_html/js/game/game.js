bc5dd7f2cc5f53f894dbee9a709ca2f1c74407be
Your Name <ant4906@yandex.ru>
2016-06-04 04:18:07 +0300

    what is it

--- public_html/js/game/game.js
+++ public_html/js/game/game.js
@@ -5,77 +5,55 @@ define(function(require) {
         Shot = require('./models/shot'),
         Spacecraft = require('./models/spacecraft');
 
-    var shots = [];
+    var URL = '/api/game';
 
-    var KEY_ONE = 49;
+    var world, status, clock, controls, player, enemy, enemyCamera, sphere;
+
+    var socket;
+
+    var shots = [];
 
     function isIntersection(boxA, boxB) {
         return boxA.intersectsBox(boxB);
     }
 
-    var Game = function(worldContainer) {
-        var game = this;
+    function render() {
+        var delta = clock.getDelta();
+        controls.update(delta);
 
-        game._url = '/api/game';
+        updatePlayer();
 
-        game._status = {
-            connected: false,
-            gaming: false
-        };
+        for (var i = 0; i < shots.length; i++) {
+            shots[i].update();
 
-        game._world = new World({
-            renderCallback: render,
-            clearColor: 0x000022,
-            container: worldContainer
-        });
-        
-        game._clock = new THREE.Clock();
+            if (isIntersection(shots[i]._hitbox, enemy._spacecraft._hitbox)) {
+                console.log('Попадание');
+                world.remove(shots[i].getMesh());
+                shots.splice(i, 1);
+            } else if (!isIntersection(shots[i]._hitbox, sphere._hitbox)) {
+                console.log('За пределами мира');
+                world.remove(shots[i].getMesh());
+                shots.splice(i, 1);
+            }
+        }    
+    };
 
-        // Sphere
-        game._world.getScene().fog = new THREE.FogExp2(0x0000022, 0.00125);        
+    function updatePlayer() {
+        var camPosition = world.getCamera().position,
+            camRotation = world.getCamera().rotation;
 
-        // Render
-        function render() {
-            var delta = game._clock.getDelta();
-            game._controls.update(delta);
-
-            game.updatePlayers();
-
-            for (var i = 0; i < shots.length; i++) {
-                shots[i].update();
-
-                if (isIntersection(shots[i]._hitbox, game._enemy._spacecraft._hitbox)) {
-                    console.log('Попадание');
-                    game._world.remove(shots[i].getMesh());
-                    shots.splice(i, 1);
-                } else if (!isIntersection(shots[i]._hitbox, game._sphere._hitbox)) {
-                    console.log('За пределами мира');
-                    game._world.remove(shots[i].getMesh());
-                    shots.splice(i, 1);
-                }
-            }    
-        };
+        player.update(camPosition, camRotation);
     }
 
-    Game.prototype.updatePlayers = function() {
-        var camPosition = this._world.getCamera().position,
-            camRotation = this._world.getCamera().rotation;
-
-        this._player.update(camPosition, camRotation);
-    }
-
-    Game.prototype.createConnection = function() {
-        var game = this;
-
-        this._socket = new WebSocket('ws://0.0.0.0:8090' + this._url);
+    function createConnection() {
+        socket = new WebSocket('ws://' + window.location.hostname + ':' + window.location.port + URL);
         
-        game._socket.onopen = function() {
+        socket.onopen = function() {
             console.log('Соединение с игровой комнатой установлено.');
-
-            game._status.connected = true;
+            status.connected = true;
         };
 
-        this._socket.onclose = function(event) {
+        socket.onclose = function(event) {
             if (event.wasClean) {
                 console.log('Соедение с игровой комнатой закрыто чисто.');
             } else {
@@ -83,59 +61,55 @@ define(function(require) {
             }
             
             console.log('Код: ' + event.code + ', причина: ' + event.reason);
-            game._status.connected = false;
-            game._status.start = false;
+            status.connected = false;
+            status.start = false;
         };
 
-        this._socket.onmessage = function(event) {
+        socket.onmessage = function(event) {
             console.log('Получены данные ' + event.data);
 
             var data = JSON.parse(event.data);
 
             if (data.start) {
-                game._status.gaming = true;
+                status.gaming = true;
                 return;
             } 
 
-            if (game._status.gaming) {
-                game._enemyCamera.position.x = data.posX;
-                game._enemyCamera.position.y = data.posY;
-                game._enemyCamera.position.z = data.posZ;
+            if (status.gaming) {
+                enemyCamera.position.x = data.posX;
+                enemyCamera.position.y = data.posY;
+                enemyCamera.position.z = data.posZ;
 
-                game._enemyCamera.rotation.x = data.rotX;
-                game._enemyCamera.rotation.y = data.rotY;
-                game._enemyCamera.rotation.z = data.rotZ;
+                enemyCamera.rotation.x = data.rotX;
+                enemyCamera.rotation.y = data.rotY;
+                enemyCamera.rotation.z = data.rotZ;
 
-                var camPosition = game._enemyCamera.position,
-                    camRotation = game._enemyCamera.rotation;
+                var camPosition = enemyCamera.position,
+                    camRotation = enemyCamera.rotation;
 
-                game._enemy.update(camPosition, camRotation);
+                enemy.update(camPosition, camRotation);
             }
         };
 
-        this._socket.onerror = function(error) {
+        socket.onerror = function(error) {
             console.log('Ошибка ' + error.message);
+            status.connected = false;
+            status.start = false;
         };
     }
 
-    Game.prototype.sendData = function() {
-        var game = this;
-
-        if (game._status.connected) {            
-            if (game._status.gaming) {
-                console.log('Send player: ' + this._player.toJSON());
-                game._socket.send(this._player.toJSON());
+    function sendData() {
+        if (status.connected) {            
+            if (status.gaming) {
+                console.log('Send player: ' + player.toJSON());
+                socket.send(player.toJSON());
             }
         } else {
-            game.createConnection();
+            createConnection();
         }
     }
 
-    Game.prototype.getWorld = function() {
-        return this._world;
-    }
-
-    Game.prototype.createShot = function(camera, player) {
+    function createShot(camera, player) {
         var raycaster = new THREE.Raycaster();
 
         var vector = new THREE.Vector3();
@@ -150,57 +124,76 @@ define(function(require) {
         );
 
         shots.push(shot);
-        this._world.add(shot.getMesh());
+        world.add(shot.getMesh());
 
         var audio = new Audio();
         audio.src = 'sounds/lazer_effect.wav';
         audio.autoplay = true;
     }
 
+    function configureControls(drag, forward, movSpeed, rollSpeed) {
+        controls.dragToLook = drag;
+        controls.autoForward = forward;
+        controls.movementSpeed =  movSpeed;
+        controls.rollSpeed = rollSpeed;
+    }
+
+    // *** //
+    var Game = function(worldContainer) {
+        status = {
+            connected: false,
+            gaming: false
+        };
+
+        world = new World({
+            renderCallback: render,
+            clearColor: 0x000022,
+            container: worldContainer
+        });
+        
+        clock = new THREE.Clock();
+
+        world.getScene().fog = new THREE.FogExp2(0x0000022, 0.00125);   
+
+
+        enemyCamera = new THREE.PerspectiveCamera(
+            45,
+            world.getContainer().width / world.getContainer().height,
+            1,
+            2000
+        );
+
+        player = new Player({posX: 0, posY: -2, posZ: -20 });
+        enemy = new Player({posX: 0, posY: -2, posZ: -20 });
+        sphere = new Sphere(1000);
+    }
+
     Game.prototype.start = function() {
-        var game = this,
-            controls = null;
-
-        game._player = new Player({posX: 0, posY: -2, posZ: -20 });
-        game._enemy = new Player({posX: 0, posY: -2, posZ: -20 });
-        game._sphere = new Sphere(1000);
-
         Promise.all([
-            game._sphere.getMesh(),
-            game._player.getMesh(),
-            game._enemy.getMesh(),
+            sphere.getMesh(),
+            player.getMesh(),
+            enemy.getMesh(),
         ]).then(function(results) {
             results.forEach(function(mesh) {
-                game._world.add(mesh);
+                world.add(mesh);
             });
 
-            game._enemyCamera = new THREE.PerspectiveCamera(
-                45,
-                game._world.getContainer().width / game._world.getContainer().height,
-                1,
-                2000
-            );
-            game._enemyCamera.add(results[2]);
-            game._world.add(game._enemyCamera);
+            enemyCamera.add(results[2]);
+            world.add(enemyCamera);
 
-            game._world.getCamera().add(results[1]);
-            game._controls = new THREE.FlyControls(game._world.getCamera(), game._world.getContainer());
+            world.getCamera().add(results[1]);
+            controls = new THREE.FlyControls(world.getCamera(), world.getContainer());
 
+            configureControls(false, false, 30, 1); // drag, forward, movSpeed, rollSpeed
 
-            game._controls.dragToLook = false;
-            game._controls.autoForward = false;
-            game._controls.movementSpeed =  30;
-            game._controls.rollSpeed = 1;
-
-            game._world.start();
-            setInterval(game.sendData.bind(game), 60);
+            world.start();
+            setInterval(sendData, 60);
 
             $(document).on('click', function (event) {
-                game.createShot(game._world.getCamera(), game._player);               
+                createShot(world.getCamera(), player);               
             })
         }); // PROMISE
     } // Game.start
-
 
     return Game;
 });
